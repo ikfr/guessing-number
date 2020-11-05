981e16a553d6624d7d420ebb861c1ee134a01fc9
hzqd <hzqelf@yeah.net>
2020-11-05 11:45:02 +0800

    elegant

---
+++ my_ext/src/kt_ext.rs
@@ -0,0 +1,35 @@
+pub trait KtStd {
+    fn let_ref<R>(&self, block: impl FnOnce(&Self) -> R) -> R {
+        block(self)
+    }
+
+    fn let_mut<R>(&mut self, mut block: impl FnMut(&mut Self) -> R) -> R {
+        block(self)
+    }
+
+    fn let_owned<R>(self, block: impl FnOnce(Self) -> R) -> R where Self: Sized {
+        block(self)
+    }
+
+    fn also_ref(&self, block: impl FnOnce(&Self)) -> &Self {
+        block(self);
+        self
+    }
+
+    fn also_mut(&mut self, mut block: impl FnMut(&mut Self)) -> &mut Self {
+        block(self);
+        self
+    }
+}
+
+impl <T> KtStd for T {}
+
+pub trait IterExt<T> {
+    fn on_each(&self, f: impl Fn(&T)) -> &Self;
+}
+
+impl<T> IterExt<T> for Vec<T> {
+    fn on_each(&self, f: impl Fn(&T)) -> &Self {
+        self.also_ref(|v| v.iter().for_each(|e| f(e)))
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use my_ext::kt_ext::KtStd;
+use rand::{thread_rng, Rng};
+use std::cmp::Ordering;
+use std::io::stdin;
+
+fn main() {
+    println!("Guess the number!");
+    thread_rng().gen_range(1, 101).let_ref(|secret_number| loop {
+        println!("Please input your guess.");
+        let guess: Option<u32> = String::new().also_mut(|guess| {
+                stdin().read_line(guess).expect("Failed to read line");
+            }).trim_end().parse().let_owned(|it| match it {
+                Ok(num) => Some(num),
+                Err(_) => { println!("wrong number"); None }
+            });
+
+        if let Some(x) = guess {
+            println!("You guessed: {}", x);
+            match x.cmp(&secret_number) {
+                Ordering::Less => println!("Too small!"),
+                Ordering::Greater => println!("Too big!"),
+                Ordering::Equal => {
+                    println!("You win!");
+                    break;
+                }
+            }
+        }
+    })
+}
